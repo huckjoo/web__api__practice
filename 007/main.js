443b23900cfea9d545267d7db13f362c2cb698e6
huckjoo <huckjoo96@naver.com>
2021-08-02 18:04:05 +0900

    Complete 007 project

--- 007/main.js
+++ 007/main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const mousePosition = document.querySelector('.mousePosition');
+const mouseRow = document.querySelector(".mouseRow");
+const mouseColumn = document.querySelector(".mouseColumn");
+const mouseImg = document.querySelector("img");
+function handleMouseMove(event){
+    mousePosition.innerHTML = `${event.clientX}px,${event.clientY}px`;
+    mousePosition.style.left = `${event.clientX+20}px`;
+    mousePosition.style.top = `${event.clientY+30}px`;
+    mouseRow.style.top = `${event.clientY}px`;
+    mouseColumn.style.left = `${event.clientX}px`
+    mouseImg.style.top = `${event.clientY}px`;
+    mouseImg.style.left = `${event.clientX}px`
+}
+
+window.addEventListener("mousemove",handleMouseMove);
