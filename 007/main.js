3d5d40b599c2461315cf1cac898f6c673f33ffff
huckjoo <huckjoo96@naver.com>
2021-08-04 16:33:03 +0900

    Revert "Revert "Increase performance""
    This reverts commit 2fd2744ab428b93fa61dfe7e3592168ab9b3ab53.

--- 007/main.js
+++ 007/main.js
@@ -4,14 +4,18 @@ const mousePosition = document.querySelector('.mousePosition');
 const mouseRow = document.querySelector(".mouseRow");
 const mouseColumn = document.querySelector(".mouseColumn");
 const mouseImg = document.querySelector("img");
+const imgHalfWidth = mouseImg.getBoundingClientRect().width/2;
+const imgHalfHeight = mouseImg.getBoundingClientRect().height/2;
+
+
+
 function handleMouseMove(event){
     mousePosition.innerHTML = `${event.clientX}px,${event.clientY}px`;
-    mousePosition.style.left = `${event.clientX+20}px`;
-    mousePosition.style.top = `${event.clientY+30}px`;
-    mouseRow.style.top = `${event.clientY}px`;
-    mouseColumn.style.left = `${event.clientX}px`
-    mouseImg.style.top = `${event.clientY}px`;
-    mouseImg.style.left = `${event.clientX}px`
+    mousePosition.style.transform = `translate(${event.clientX+15}px,${event.clientY+25}px)`;
+    mouseRow.style.transform = `translate(0,${event.clientY}px)`;
+    mouseColumn.style.transform = `translate(${event.clientX}px)`
+    mouseImg.style.transform = `translate(${event.clientX}px,${event.clientY}px) translate(-50%,-50%)`;
+
 }
 
 window.addEventListener("mousemove",handleMouseMove);
