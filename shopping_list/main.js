6568d014475f68e7e2c995c5f5001505b70b584f
huckjoo <huckjoo96@naver.com>
2021-08-10 17:37:50 +0900

    Refactoring shopping list

--- shopping_list/main.js
+++ shopping_list/main.js
@@ -13,27 +13,30 @@ function handleSubmit(event){
     }
     input.value="";
     input.focus();
-    const ul = document.querySelector(".main__list");
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const button = document.createElement("button");
-    button.innerHTML=`<i class="delBtn fas fa-trash"></i>`;
-    button.setAttribute("class","list__delBtn")
-    span.innerHTML = `${value}`;
-    span.setAttribute("class","list__text");
-    span.appendChild(button);
-
-    li.setAttribute("class","list__item");
-    li.appendChild(span);
-    mainLists.appendChild(li);
-    button.addEventListener("click",()=>{
-        ul.removeChild(li);//수많은 li중에 어떻게 그 li인지 알지? 클로저때문
-    })
-    li.scrollIntoView();//이것도 클로저 때문인가?
+    createList(value);
     // const delBtn = document.querySelectorAll(".delBtn");
     // delBtn.forEach((x)=>x.addEventListener("click",handleDelete));
 
     }
+    let id=0;
+function createList(value){
+    const li = document.createElement("li");
+    li.setAttribute("class","list__item");
+    li.setAttribute("data-id",id);
+    li.innerHTML = `
+        <span class="list__text">${value}
+            <button class="list__delBtn">
+                <i class="delBtn fas fa-trash" data-id=${id}></i>
+            </button>
+        </span>
+        `
+    mainLists.appendChild(li);
+    // button.addEventListener("click",()=>{
+    //     ul.removeChild(li);//수많은 li중에 어떻게 그 li인지 알지? 클로저때문
+    // })
+    li.scrollIntoView();//이것도 클로저 때문인가?
+    id++;
+}
 function allKill(){
     const ul = document.querySelector(".main__list");
     while(ul.firstChild){
@@ -43,5 +46,11 @@ function allKill(){
 inputForm.addEventListener("submit",handleSubmit);
 addBtn.addEventListener("click",handleSubmit);
 allKillBtn.addEventListener("click",allKill);
-
+mainLists.addEventListener("click",(event)=>{
+    const id = event.target.dataset.id;
+    if(id){
+        const tobeDeleted = document.querySelector(`.list__item[data-id="${id}"]`);//왜 ${id}값에 ""를 붙여야 하는가?
+        tobeDeleted.remove();
+    }
+})
 
